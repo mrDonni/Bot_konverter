c299db4050bb61e57475f940c218f1129f94da86
mrDonni <75480310+mrDonni@users.noreply.github.com>
2021-01-10 08:12:24 +0300

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -1,3 +1,8 @@
+import json
+import requests
+
+from config import  currency
+
 class Bot_exception(BaseException):
     pass
 
@@ -7,4 +12,9 @@ class Converter:
     def get_price(user_input):
         if len(user_input) !=3:
             raise Bot_exception
-        base, quote, ammount = user_input
+        quote, base, ammount = user_input
+
+        r = requests.get(f'https://api.exchangeratesapi.io/latest?base={currency[base]}&symbols={currency[quote]}')
+        data = json.loads(r.content)['rates'][currency[quote]]
+        result = round(float(data) * float(ammount), 3)
+        return result
