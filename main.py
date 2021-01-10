91c2df1d5550906eac74cd51979619bc033de09f
mrDonni <75480310+mrDonni@users.noreply.github.com>
2021-01-10 08:41:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import telebot
 import json
 import requests
-from config import TOKEN
+from config import TOKEN, currency
 from extensions import Converter
 
 
@@ -15,18 +15,24 @@ def greeting(message):
 @bot.message_handler(commands=['help','start'])
 def help(message: telebot.types.Message):
     bot.send_message(message.chat.id, f'{message.from_user.first_name} , для \
-работы воспользуйтесь одним из методов , введите /values для списка доступных валют' )
+работы воспользуйтесь одним из методов\
+b (пока работает только 1),\
+введите валюты в формате <валюта на которую меняют> <валюта которую меняют> <количество>\
+\в групповых чатах команда начинается с /\
+\nвведите /values для списка доступных валют\
+\nвведите /hello для приветствия' )
 
 @bot.message_handler(commands=['values'])
 def values(message: telebot.types.Message):
     text = 'Доступные валюты:'
     for cur in currency.keys():
-        text = '\n'.join((text, cur, ))
+        text = str.title('\n'.join((text, cur, )))
     bot.reply_to(message, text)
 
 @bot.message_handler(content_types=['text',])
 def convertion(message: telebot.types.Message):
-    user_input = list(map(str.lower, message.text.split(' ')))
+    a = message.text.replace('/','')
+    user_input = list(map(str.lower, a.split(' ')))
 
 
     bot.send_message(message.chat.id, Converter.get_price(user_input))
