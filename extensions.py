b78d3628616281bd42a1fa5c4d18673f8cdb4611
mrDonni <75480310+mrDonni@users.noreply.github.com>
2021-01-11 05:59:44 +0300

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -11,8 +11,19 @@ class Converter:
     @staticmethod
     def get_price(user_input):
         if len(user_input) !=3:
-            raise Bot_exception
+            raise Bot_exception("Введите правильное количество параметров")
+
         quote, base, ammount = user_input
+
+        if quote == base :
+            raise Bot_exception("Невозможно поменять валюту саму на себя")
+
+        if quote and base  not in currency:
+            raise Bot_exception('Введите валюту доступную для перевода')
+        try:
+            ammount = float(ammount)
+        except ValueError:
+            raise Bot_exception(f'Неудалось обработать количество{ammount}')
 
         r = requests.get(f'https://api.exchangeratesapi.io/latest?base={currency[base]}&symbols={currency[quote]}')
         data = json.loads(r.content)['rates'][currency[quote]]
