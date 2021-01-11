6734479ee531f56a196694dd440c20010a156a19
mrDonni <75480310+mrDonni@users.noreply.github.com>
2021-01-11 06:20:05 +0300

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -10,6 +10,7 @@ class Bot_exception(BaseException):
 class Converter:
     @staticmethod
     def get_price(user_input):
+
         if len(user_input) !=3:
             raise Bot_exception("Введите правильное количество параметров")
 
@@ -23,9 +24,9 @@ class Converter:
         try:
             ammount = float(ammount)
         except ValueError:
-            raise Bot_exception(f'Неудалось обработать количество{ammount}')
+            raise Bot_exception(f'Неудалось обработать количество {ammount}')
 
         r = requests.get(f'https://api.exchangeratesapi.io/latest?base={currency[base]}&symbols={currency[quote]}')
         data = json.loads(r.content)['rates'][currency[quote]]
-        result = round(float(data) * float(ammount), 3)
-        return f'{result} {quote} в {ammount} {base}'
+        result = round(float(data) * float(ammount), 2)
+        return f'{ammount} {str.title(base)} равняется {result} {str.title(quote)}'
